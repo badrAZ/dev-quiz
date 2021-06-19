69d7f489831773dcdf8f5d6900c9a65159d78a2a
azizbibadr@gmail.com <azizbibadr@gmail.com>
2021-06-19 21:40:09 +0200

    feat(web): improve quiz form

--- web/src/components/quiz.js
+++ web/src/components/quiz.js
@@ -1,12 +1,27 @@
+import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
+import Form from 'react-bootstrap/Form'
 import React from 'react'
 import { BsPuzzleFill } from 'react-icons/bs'
+import { RiAlarmWarningFill } from 'react-icons/ri'
 
-import SelectQuizType from './selectQuizType'
+import NoData from '../common/noData'
+import randomId from '../common/randomId'
+
+import FetchQuiz from './fetchQuiz'
 import QuizForm from './quizForm'
+import SelectQuizType from './selectQuizType'
 
 export default function Quiz() {
   const [type = 'javascript', setType] = React.useState()
+  const { error, quiz } = FetchQuiz(type)
+  const [quizAnswers, setQuizAnswers] = React.useState({})
+  const [formId] = React.useState(randomId())
+
+  const validate = React.useCallback(event => {
+    event.preventDefault()
+    console.log('To implement')
+  }, [])
 
   return (
     <Card>
@@ -17,8 +32,31 @@ export default function Quiz() {
         <SelectQuizType onChange={setType} value={type} />
       </Card.Header>
       <Card.Body>
-        <QuizForm type={type} />
+        {error !== undefined ? (
+          <div className='text-danger'>
+            <RiAlarmWarningFill /> {error}
+          </div>
+        ) : (
+          <NoData data={quiz}>
+            {() => (
+              <Form id={formId} onSubmit={validate}>
+                <QuizForm
+                  quiz={quiz}
+                  quizAnswers={quizAnswers}
+                  setQuizAnswers={setQuizAnswers}
+                />
+              </Form>
+            )}
+          </NoData>
+        )}
       </Card.Body>
+      <Card.Footer className='text-md-center'>
+        {quiz !== undefined && (
+          <Button type='submit' variant='success' form={formId}>
+            Validate
+          </Button>
+        )}
+      </Card.Footer>
     </Card>
   )
 }
--- web/src/components/quizForm.js
+++ web/src/components/quizForm.js
@@ -1,63 +1,90 @@
 import Button from 'react-bootstrap/Button'
 import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
+import Badge from 'react-bootstrap/Badge'
+import Form from 'react-bootstrap/Form'
 import PropTypes from 'react-proptypes'
 import React from 'react'
 import Row from 'react-bootstrap/Row'
-import { RiAlarmWarningFill } from 'react-icons/ri'
+import { MdQuestionAnswer } from 'react-icons/md'
 
-import NoData from '../common/noData'
+function SubForm({ id, question, answers }) {
+  return [
+    <Row className='mb-1' key='question'>
+      <Col>
+        <MdQuestionAnswer size={20} /> {question}
+      </Col>
+    </Row>,
+    ...Object.entries(answers).map(([value, answer]) => (
+      <Row key={value}>
+        <Col>
+          <Form.Check label={answer} type='radio' id={value} />
+        </Col>
+      </Row>
+    )),
+  ]
+}
 
-import FetchQuiz from './fetchQuiz'
-
-export default function QuizForm({ type }) {
-  const { error, quiz } = FetchQuiz(type)
-  const [pos, setPos] = React.useState(0)
-
+function QuizPagination({ pos, setPos, nQuestions }) {
   const next = React.useCallback(() => {
-    if (pos < Object.keys(quiz).length - 1) {
+    if (pos < nQuestions - 1) {
       setPos(pos + 1)
     }
-  }, [pos, quiz])
+  }, [pos, nQuestions, setPos])
   const back = React.useCallback(() => {
     if (pos > 0) {
       setPos(pos - 1)
     }
-  }, [pos])
+  }, [pos, setPos])
 
-  return error !== undefined ? (
-    <div className='text-danger'>
-      <RiAlarmWarningFill /> {error}
-    </div>
-  ) : (
-    <NoData data={quiz}>
-      {() => (
-        <Container fluid>
-          <Row>
-            <Col>{JSON.stringify(Object.values(quiz)[pos])}</Col>
-          </Row>
-          <Row>
-            <Col md={{ span: 6 }}>
-              <Button size='sm' disabled={pos === 0} onClick={back}>
-                Back
-              </Button>
-            </Col>
-            <Col md={{ offset: 4 }}>
-              <Button
-                disabled={pos === Object.keys(quiz).length - 1}
-                onClick={next}
-                size='sm'
-              >
-                Next
-              </Button>
-            </Col>
-          </Row>
-        </Container>
-      )}
-    </NoData>
+  return (
+    <Row>
+      <Col>
+        <Button size='sm' disabled={pos === 0} onClick={back}>
+          Back
+        </Button>
+        <Button
+          className='float-md-right'
+          disabled={pos === nQuestions - 1}
+          onClick={next}
+          size='sm'
+        >
+          Next
+        </Button>
+      </Col>
+    </Row>
+  )
+}
+
+export default function QuizForm({ quiz, quizAnswers, setQuizAnswers }) {
+  const [pos, setPos] = React.useState(0)
+  const nQuestions = Object.keys(quiz).length
+
+  const currentQuiz = React.useMemo(() => {
+    const [id, currentQuiz] = Object.entries(quiz)[pos]
+    return {
+      id,
+      ...currentQuiz,
+    }
+  }, [quiz, pos])
+
+  return (
+    <Container fluid>
+      <Row>
+        <Col>
+          <Badge className='float-md-right' variant='primary'>
+            {pos + 1}/{nQuestions}
+          </Badge>
+        </Col>
+      </Row>
+      <SubForm {...currentQuiz} />
+      <QuizPagination pos={pos} setPos={setPos} nQuestions={nQuestions} />
+    </Container>
   )
 }
 
 QuizForm.propTypes = {
-  type: PropTypes.string.isRequired,
+  quiz: PropTypes.object.isRequired,
+  quizAnswers: PropTypes.object.isRequired,
+  setQuizAnswers: PropTypes.func.isRequired,
 }
